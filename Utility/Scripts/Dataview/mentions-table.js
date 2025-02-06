6f383d3fd455c59b4f07d34c575603b0a89e6a9c
hacker <maxime@glaizot.net>
2025-02-06 11:57:14 +0100

    update People script to get the task list

--- Utility/Scripts/Dataview/mentions-table.js
+++ Utility/Scripts/Dataview/mentions-table.js
@@ -1,12 +1,28 @@
+const theName = dv.current().file.name.replace(/^@/, '')
+
+
+
 if (input) {
 	// Add a header of level X, e.g. input = 2 means header level 2
 	const name = dv.current().file.name.replace(/^@/, '')
-	dv.header(input, 'Notes referencing ' + name)
+	dv.header(input, 'Notes referencing ' + theName)
 }
+
+
+
+dv.taskList(dv.pages().file.tasks
+    .where(t => t.text.includes(theName))
+    .where(t => !t.completed),false
+    )
+
+	
+
 dv.table(
 	["Note", "Location"],
-	dv.pages("[[" + dv.current().file.name + "]]")
+	dv.pages("[[" + theName + "]]")
 		// Sort by YAML created field, but check whether there are multiple created dates
 		.sort(b => moment(Array.isArray(b.created) ? b.created[0] : b.created), 'desc')
 		.map(b => [b.file.link, b.file.folder.replace(/\//g, ' ‣ ').replace(/^\d+ (.+)/, '$1')])
 )
+
+
